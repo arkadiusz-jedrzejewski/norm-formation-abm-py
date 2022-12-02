07bfa5b94c6e0f27dbe94da8aab6139016e6030f
Arkadiusz <arekjd@gmail.com>
2022-12-02 09:54:11 +0100

    getting directory list added

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import numpy as np
 import os
-
+from datetime import date
 
 if __name__ == "__main__":
-    print("Hello")
-    os.system("norm_formation_abm.exe 10 0.2 3 0.5 10000 1 200 name.txt")
+
+    today = date.today()
+    print(f"Today is {today}")
+    print(type(today.strftime("%Y-%m-%d")))
+    print(today.strftime("%y%m%d"))
+
+    print(os.listdir())
+    dirlist = [item for item in os.listdir() if os.path.isdir(item)]
+    print(dirlist)
+
+    # os.system("norm_formation_abm.exe 10 0.2 3 0.5 10000 1 200 name.txt")
