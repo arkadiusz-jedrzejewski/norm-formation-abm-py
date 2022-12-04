85cad12353eb5dc9314bef8fdbbd7553201067e8
Arkadiusz <arekjd@gmail.com>
2022-12-05 00:22:21 +0100

    dir_name generation added

--- main.py
+++ main.py
@@ -12,9 +12,23 @@ if __name__ == "__main__":
     print(os.listdir())
     dirlist = [item for item in os.listdir() if os.path.isdir(item)]
     print(dirlist)
+    res = []
     for item in dirlist:
         print(item)
         print(item.split("-"))
+        items = item.split("-")
+        if len(items) == 3 and items[1] == "sim":
+            if today.strftime("%y%m%d") == items[0]:
+                print("ll")
+                res.append(items[2])
+
+    next_index = 1
+    if len(res) != 0:
+        next_index = int(max(res)) + 1
+    print(next_index)
+    dir_name = today.strftime("%y%m%d") + '-sim-' + str(next_index)
+    print(dir_name)
+    os.mkdir(dir_name)
 
 
     # os.system("norm_formation_abm.exe 10 0.2 3 0.5 10000 1 200 name.txt")
