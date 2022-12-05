9f87c8ded95303f9918ebb2941af3f737963f967
Arkadiusz <arekjd@gmail.com>
2022-12-06 00:29:56 +0100

    average over simulations added - only concentration

--- plot_phase_diagram.py
+++ plot_phase_diagram.py
@@ -11,15 +11,17 @@ print(ps)
 sim_num = np.loadtxt(dir_name + "/sim_num.csv", dtype=int)
 print(sim_num)
 
-cs = np.zeros(ps.shape)
+cs = np.zeros((len(ps), sim_num))
+print(cs.shape)
 plt.figure(1)
 for i in range(len(ps)):
-    single_sim = np.loadtxt(dir_name + f"/{i}/sim-{1}.txt")
-    cs[i] = max(single_sim[-1], 1 - single_sim[-1])
-    plt.plot(single_sim)
-    print(single_sim)
+    for sim_number in range(sim_num):
+        single_sim = np.loadtxt(dir_name + f"/{i}/sim-{sim_number}.txt")
+        cs[i, sim_number] = max(single_sim[-1], 1 - single_sim[-1])
+    #plt.plot(single_sim)
+    #print(single_sim)
 
-print(cs)
+print(np.mean(cs, axis=1))
 plt.figure(2)
 plt.plot(ps, cs, ".-")
 plt.xlabel("nonconformity probability")
