6c2fcc97b1416f5ef765931a6800b3ee7a575fbe
Arkadiusz <arekjd@gmail.com>
2022-12-05 14:39:56 +0100

    plotting phase diagram from single simulations

--- plot_phase_diagram.py
+++ plot_phase_diagram.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 print("phase diagram")
 dir_name = "221205-sim-1"
@@ -9,4 +10,19 @@ print(ps)
 
 sim_num = np.loadtxt(dir_name + "/sim_num.csv", dtype=int)
 print(sim_num)
+
+cs = np.zeros(ps.shape)
+plt.figure(1)
+for i in range(len(ps)):
+    single_sim = np.loadtxt(dir_name + f"/{i}/sim-{0}.txt")
+    cs[i] = single_sim[-1]
+    plt.plot(single_sim)
+    print(single_sim)
+
+print(cs)
+plt.figure(2)
+plt.plot(ps, cs, ".-")
+plt.xlabel("nonconformity probability")
+plt.ylabel("concentration")
+plt.show()
 
