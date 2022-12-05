2ed96dbb73081547b1bc15438c1a2b8a9d331cf1
Arkadiusz <arekjd@gmail.com>
2022-12-05 09:57:43 +0100

    get_dir_name function added

--- main.py
+++ main.py
@@ -2,33 +2,30 @@ import numpy as np
 import os
 from datetime import date
 
-if __name__ == "__main__":
 
+def get_dir_name():
     today = date.today()
-    print(f"Today is {today}")
-    print(type(today.strftime("%Y-%m-%d")))
-    print(today.strftime("%y%m%d"))
-
-    print(os.listdir())
     dirlist = [item for item in os.listdir() if os.path.isdir(item)]
-    print(dirlist)
     res = []
+
     for item in dirlist:
-        print(item)
-        print(item.split("-"))
         items = item.split("-")
         if len(items) == 3 and items[1] == "sim":
             if today.strftime("%y%m%d") == items[0]:
-                print("ll")
                 res.append(items[2])
 
     next_index = 1
     if len(res) != 0:
         next_index = int(max(res)) + 1
-    print(next_index)
+
     dir_name = today.strftime("%y%m%d") + '-sim-' + str(next_index)
+
+    return dir_name
+
+
+if __name__ == "__main__":
+    dir_name = get_dir_name()
     print(dir_name)
     os.mkdir(dir_name)
-
 
     # os.system("norm_formation_abm.exe 10 0.2 3 0.5 10000 1 200 name.txt")
