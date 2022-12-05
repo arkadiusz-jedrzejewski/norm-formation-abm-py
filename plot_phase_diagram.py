3a0ed5113c1debaa0a99c231a370ba05af70e96f
Arkadiusz <arekjd@gmail.com>
2022-12-05 14:18:59 +0100

    load data added

--- plot_phase_diagram.py
+++ plot_phase_diagram.py
@@ -1,1 +1,12 @@
+import numpy as np
+
 print("phase diagram")
+dir_name = "221205-sim-1"
+
+probs = np.loadtxt(dir_name + "/probs.csv")
+ps = probs[:, 1]
+print(ps)
+
+sim_num = np.loadtxt(dir_name + "/sim_num.csv", dtype=int)
+print(sim_num)
+
