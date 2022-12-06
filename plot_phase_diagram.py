8e06ed3011f64c0670d86e743c7a313adca54d00
Arkadiusz <arekjd@gmail.com>
2022-12-06 10:07:54 +0100

    get_sem function added -> returns standard error of the mean, plotting averaged phase diagram added

--- plot_phase_diagram.py
+++ plot_phase_diagram.py
@@ -1,8 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+def get_sem(data):
+    """
+    returns standard error of the mean
+    """
+    return np.std(data, axis=1, ddof=1) / np.sqrt(np.size(data, axis=1))
+
+
 print("phase diagram")
-dir_name = "221205-sim-1"
+dir_name = "221206-sim-2"
 
 probs = np.loadtxt(dir_name + "/probs.csv")
 ps = probs[:, 1]
@@ -18,13 +26,17 @@ for i in range(len(ps)):
     for sim_number in range(sim_num):
         single_sim = np.loadtxt(dir_name + f"/{i}/sim-{sim_number}.txt")
         cs[i, sim_number] = max(single_sim[-1], 1 - single_sim[-1])
-    #plt.plot(single_sim)
-    #print(single_sim)
+        plt.plot(single_sim)
 
-print(np.mean(cs, axis=1))
+
+print(get_sem(cs))
 plt.figure(2)
 plt.plot(ps, cs, ".-")
 plt.xlabel("nonconformity probability")
 plt.ylabel("concentration")
-plt.show()
 
+plt.figure(3)
+plt.errorbar(ps, np.mean(cs, axis=1), yerr=get_sem(cs), fmt='.-')
+plt.xlabel("nonconformity probability")
+plt.ylabel("concentration")
+plt.show()
