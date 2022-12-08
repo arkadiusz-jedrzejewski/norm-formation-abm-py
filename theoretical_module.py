968497ecc62b19288b64a844d13ffd34c022c31f
Arkadiusz <arekjd@gmail.com>
2022-12-08 16:42:40 +0100

    annealed option added to get_fixed_points function

--- theoretical_module.py
+++ theoretical_module.py
@@ -11,13 +11,18 @@ def nonconformity_function(x, f):
     return f
 
 
-def get_fixed_points(num, q, f):
+def get_fixed_points(num, q, f, is_quenched=False):
     cs = np.linspace(0, 1, num=num)
-    # quenched model
-    numerator = cs * (conformity_function(1 - cs, q) + conformity_function(cs, q)) - conformity_function(cs, q)
-    denominator = nonconformity_function(cs, f) * (
-            conformity_function(1 - cs, q) + conformity_function(cs, q)) - conformity_function(cs, q)
-    ps = numerator / denominator
+    if is_quenched:
+        # quenched model
+        numerator = cs * (conformity_function(1 - cs, q) + conformity_function(cs, q)) - conformity_function(cs, q)
+        denominator = nonconformity_function(cs, f) * (
+                conformity_function(1 - cs, q) + conformity_function(cs, q)) - conformity_function(cs, q)
+        ps = numerator / denominator
+    else:
+        numerator = cs * conformity_function(1 - cs, q) - (1 - cs) * conformity_function(cs, q)
+        ps = numerator / (nonconformity_function(cs, f) - cs + numerator)
+
 
     return ps, cs
 
@@ -26,7 +31,7 @@ ps, cs = get_fixed_points(num=100,
                           q=3,
                           f=0.5)
 
-plt.figure(1)
-plt.plot(ps, cs, '-')
-
-plt.show()
+# plt.figure(1)
+# plt.plot(ps, cs, '-')
+#
+# plt.show()
