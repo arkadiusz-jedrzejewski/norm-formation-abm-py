8581b1732c71335a539f2842ac71f304b90dfa02
Arkadiusz <arekjd@gmail.com>
2022-12-09 11:56:59 +0100

    added q-voter model with independence

--- theoretical_module.py
+++ theoretical_module.py
@@ -23,13 +23,26 @@ def get_fixed_points(num, q, f, is_quenched=False):
         numerator = cs * conformity_function(1 - cs, q) - (1 - cs) * conformity_function(cs, q)
         ps = numerator / (nonconformity_function(cs, f) - cs + numerator)
 
+    return ps, cs
+
+
+def get_fixed_points_q_voter(num, q, f, is_quenched=False):
+    cs = np.linspace(0, 1, num=num)
+    if is_quenched:
+        # quenched model
+        numerator = (cs * (1 - cs) ** q - (1 - cs) * cs ** q)
+        ps = 2 * numerator / ((1 - cs) ** q + cs ** q)
+    else:
+        # annealed model
+        numerator = cs * (1 - cs) ** q - (1 - cs) * cs ** q
+        ps = numerator / ((1 - 2 * cs) * f + numerator)
 
     return ps, cs
 
 
-ps, cs = get_fixed_points(num=100,
-                          q=3,
-                          f=0.5)
+ps, cs = get_fixed_points_q_voter(num=100,
+                                  q=3,
+                                  f=0.5)
 
 # plt.figure(1)
 # plt.plot(ps, cs, '-')
