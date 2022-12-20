d6c6c830f1a2ef201ed0998a4ae87cc2396e839b
Arkadiusz <arekjd@gmail.com>
2022-12-20 11:26:25 +0100

    function get_phase_diagram added with error tolerance

--- theoretical_module.py
+++ theoretical_module.py
@@ -2,7 +2,7 @@
 import numpy as np
 from scipy.special import lambertw
 import scipy.integrate as integrate
-from scipy.optimize import minimize, Bounds
+from scipy.optimize import minimize, Bounds, fsolve
 import matplotlib.pyplot as plt
 
 
@@ -63,24 +63,32 @@ def get_fixed_points_q_voter(num, q, f, is_quenched=False):
 
 
 def fun(x, c, f, q):
+    # nonconformity_function -> probability of engaging (opinion changes to 1)
     numerator = x * nonconformity_function(c, f) + (1 - x) * conformity_function(c, q)
     denominator = x + (1 - x) * (conformity_function(1 - c, q) + conformity_function(c, q))
     return numerator / denominator
 
 
-p_start = 0
-p_stop = 1
-p_num = 400
-ps = np.linspace(p_start, p_stop, p_num)
-res = np.zeros(ps.shape)
-bnd = Bounds(lb=0.51, ub=1)
+def get_phase_diagram(p_start, p_stop, p_num, q, tol):
+    ps = np.linspace(p_start, p_stop, p_num)
+    cs = np.zeros(ps.shape)
+    for i, p in enumerate(ps):
+        c = fsolve(lambda x: x - integrate.quad(fun, 0, p, (x, 0.5, q))[0] / p,
+                   cs[i - 1] if i > 0 else 1)
+        cs[i] = c
+        # error
+        if np.abs(c - integrate.quad(fun, 0, p, (c, 0.5, q))[0] / p) > tol:
+            cs[i] = np.NAN
 
-for i, p in enumerate(ps):
-    sol = minimize(lambda x: np.abs(x - integrate.quad(fun, 0, p, (x, 0.5, 3))[0] / p),
-                   res[i - 1] if i > 0 else 1,
-                   tol=1e-6, bounds=bnd)
-    res[i] = sol.x[0]
-    print(i, res[i], sol.success, sol.message)
+    return ps, cs
 
-plt.plot(ps, res, '.-')
+
+ps, cs = get_phase_diagram(p_start=0,
+                           p_stop=0.4,
+                           p_num=20,
+                           q=8,
+                           tol=1e-6)
+
+print(ps,cs,sep='\n')
+plt.plot(ps, cs, '.-')
 plt.show()
