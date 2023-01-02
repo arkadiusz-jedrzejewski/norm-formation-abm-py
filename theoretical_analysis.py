91a056764ec7d25afe1ff5d5dde7633d9cc88056
Arkadiusz <arekjd@gmail.com>
2023-01-02 14:00:40 +0100

    theoretical module executing script -> theoretical analysis

--- theoretical_analysis.py
+++ theoretical_analysis.py
@@ -1,20 +1,23 @@
 import matplotlib.pyplot as plt
-from theoretical_module import get_fixed_points, get_fixed_points_q_voter
+from theoretical_module import get_fixed_points, Power, Logistic
 
-num = 100
-q = 3
-f = 0.5
+q = 1.5
+x0 = 0.5
+k = 0
+m = 0.6
+is_quenched = True
 
-ps, cs = get_fixed_points(num=num,
-                          q=q,
-                          f=f,
-                          is_quenched=False)
-plt.figure(1)
-plt.plot(ps, cs, '-')
-ps, cs = get_fixed_points(num=num,
-                          q=q,
-                          f=f,
-                          is_quenched=True)
-plt.plot(ps, cs, '-')
+conf_fun = Power(q=q)
+print(conf_fun)
+
+nonconf_fun = Logistic(x0=x0, k=k, m=m)
+print(nonconf_fun)
+
+ps, cs = get_fixed_points(100, conf_fun, nonconf_fun, is_quenched=is_quenched)
+plt.plot(ps, cs)
 plt.xlim([0, 1])
+plt.ylim([0, 1])
+plt.title(conf_fun.__str__() + " " + nonconf_fun.__str__() + " " + f"is_quenched={is_quenched}")
+plt.xlabel("nonconformity")
+plt.ylabel("concentration")
 plt.show()
--- theoretical_module.py
+++ theoretical_module.py
@@ -230,25 +230,7 @@ def get_roots(f, a, b, dx) -> list:
     return roots
 
 
-q = 1.5
-x0 = 0.5
-k = 0
-m = 0.5
-is_quenched = True
 
-conf_fun = SymmetricPower(q=q)
-print(conf_fun)
-
-nonconf_fun = Logistic(x0=x0, k=k, m=m)
-print(nonconf_fun)
-
-ps, cs = get_fixed_points(100, conf_fun, nonconf_fun, is_quenched=is_quenched)
-plt.plot(ps, cs)
-plt.xlim([0, 1])
-plt.ylim([0, 1])
-plt.xlabel("nonconformity")
-plt.ylabel("concentration")
-plt.show()
 # plt.plot(conc, nonf.get(conc))
 # plt.ylim((0, 1))
 # plt.show()
