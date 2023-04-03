numerical determination of fixed points improved (accounts for 0.5)

theoretical_analysis.py
@@ -16,7 +16,7 @@ from matplotlib.animation import PillowWriter
 
 
 k_tab = np.linspace(-80, 10, 200)
-plot_fixed_points_k(k_tab=k_tab, q=4, p=0.2)
+plot_fixed_points_k(k_tab=k_tab, q=3, p=0.2, m=0.6, is_quanched=True)
 
 # ## movie plot
 #