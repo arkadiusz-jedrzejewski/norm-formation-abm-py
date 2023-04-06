1fe36ce9ac6ad1c908a9c432a892db7db54a9dbc
Arkadiusz <arekjd@gmail.com>
2023-04-06 16:39:14 +0200

    run simulations to plot diagrams wrt p, k, m

--- main.py
+++ main.py
@@ -26,69 +26,120 @@ def get_dir_name():
     return dir_name
 
 
-def run_single_sim(p_tuple, is_annealed, q, x0, k, m, system_size, init_opinions, time_horizon, p_dir_name):
-    p_index, p, sim_number, seed = p_tuple
-    file_name = p_dir_name + f"/{p_index}/sim-{sim_number}.txt"
+def run_single_sim(arg0_tuple, is_annealed, q, x0, arg1, arg2, system_size, init_opinions, time_horizon, p_dir_name,
+                   mode):
+    if mode == 0:
+        arg0_index, p, sim_number, seed = arg0_tuple
+        k = arg1
+        m = arg2
+    elif mode == 1:
+        arg0_index, k, sim_number, seed = arg0_tuple
+        p = arg1
+        m = arg2
+    elif mode == 2:
+        arg0_index, m, sim_number, seed = arg0_tuple
+        p = arg1
+        k = arg2
+
+    file_name = p_dir_name + f"/{arg0_index}/sim-{sim_number}.txt"
     is_annealed = 1 if is_annealed else 0
     subprocess.run(
         f"norm_formation_abm.exe {file_name} {seed} {system_size} {time_horizon} {is_annealed} {init_opinions} {q} {x0} {k} {m} {p}")
 
 
-def create_diagram_p(p_start, p_stop, p_num, is_annealed, sim_num, q, x0, k, m, system_size, time_horizon, seed):
-    ps = np.linspace(p_start, p_stop, p_num)
-    ps_index = np.arange(p_num)
+def create_diagram(p, is_annealed, sim_num, q, x0, k, m, system_size, time_horizon,
+                   seed):
+    if type(p) is tuple:
+        arg_start, arg_stop, arg_num = p
+        header = "mode, p_start, p_stop, p_num, sim_num, q, x0, k, m, time_horizon, system_size, is_annealed"
+        arg1 = k
+        arg2 = m
+        mode = 0
+    elif type(k) is tuple:
+        arg_start, arg_stop, arg_num = k
+        header = "mode, k_start, k_stop, k_num, sim_num, q, x0, p, m, time_horizon, system_size, is_annealed"
+        arg1 = p
+        arg2 = m
+        mode = 1
+    elif type(m) is tuple:
+        arg_start, arg_stop, arg_num = m
+        header = "mode, m_start, m_stop, m_num, sim_num, q, x0, p, k, time_horizon, system_size, is_annealed"
+        arg1 = p
+        arg2 = k
+        mode = 2
+
+    args = np.linspace(arg_start, arg_stop, arg_num)
+    args_index = np.arange(arg_num)
 
     sims = np.arange(sim_num)
 
-    np.savetxt(dir_name + "/probs.csv", np.column_stack((ps_index, ps)), fmt="%i, %.18f")
+    np.savetxt(dir_name + "/args.csv", np.column_stack((args_index, args)), fmt="%i, %.18f")
     np.savetxt(dir_name + "/sim_num.csv", [sim_num], fmt="%i")
     np.savetxt(dir_name + "/params.csv",
-               [[p_start, p_stop, p_num, sim_num, q, x0, k, m, time_horizon, system_size, is_annealed]],
-               fmt="%.8f, " * 2 + "%i, %i, %.8f, %.8f, %.8f, %.8f, %i, %i, %i",
-               header="p_start, p_stop, p_num, sim_num, q, x0, k, m, time_horizon, system_size, is_annealed")
+               [[mode, arg_start, arg_stop, arg_num, sim_num, q, x0, arg1, arg2, time_horizon, system_size,
+                 is_annealed]],
+               fmt="%i, " + "%.8f, " * 2 + "%i, %i, %.8f, %.8f, %.8f, %.8f, %i, %i, %i",
+               header=header)
 
-    p_tuples = []
+    arg0_tuples = []
 
-    for p_index in ps_index:
-        if not os.path.exists(dir_name + f"/{p_index}"):
-            os.mkdir(dir_name + f"/{p_index}")
+    for arg_index in args_index:
+        if not os.path.exists(dir_name + f"/{arg_index}"):
+            os.mkdir(dir_name + f"/{arg_index}")
         seeds = []
         for sim_number in sims:
-            p_tuples.append((p_index, ps[p_index], sim_number, seed))
+            arg0_tuples.append((arg_index, args[arg_index], sim_number, seed))
             seeds.append(seed)
             seed += 1000
-        np.savetxt(dir_name + f"/{p_index}/seeds.csv", np.column_stack((sims, seeds)), fmt="%i, %i")
+        np.savetxt(dir_name + f"/{arg_index}/seeds.csv", np.column_stack((sims, seeds)), fmt="%i, %i")
 
     with mp.Pool(6) as pool:
         pool.map(partial(run_single_sim,
                          is_annealed=is_annealed,
                          q=q,
                          x0=x0,
-                         k=k,
-                         m=m,
+                         arg1=arg1,
+                         arg2=arg2,
                          system_size=system_size,
                          init_opinions=1,
                          time_horizon=time_horizon,
-                         p_dir_name=dir_name
+                         p_dir_name=dir_name,
+                         mode=mode
                          ),
-                 p_tuples)
+                 arg0_tuples)
 
 
 if __name__ == "__main__":
     dir_name = get_dir_name()
     os.mkdir(dir_name)
 
-    create_diagram_p(p_start=0,
-                     p_stop=1,
-                     p_num=25,
-                     is_annealed=True,
-                     sim_num=2,
-                     q=3,
-                     x0=0.5,
-                     k=-10,
-                     m=0.6,
-                     system_size=10000,
-                     time_horizon=1000,
-                     seed=1)
-
-
+    # create_diagram(p=0.3,
+    #                is_annealed=True,
+    #                sim_num=2,
+    #                q=3,
+    #                x0=0.5,
+    #                k=(-50, 20, 25),
+    #                m=0.6,
+    #                system_size=10000,
+    #                time_horizon=1000,
+    #                seed=1)
+    # create_diagram(p=(0, 1, 25),
+    #                is_annealed=True,
+    #                sim_num=2,
+    #                q=3,
+    #                x0=0.5,
+    #                k=-10,
+    #                m=0.6,
+    #                system_size=10000,
+    #                time_horizon=1000,
+    #                seed=1)
+    create_diagram(p=0.3,
+                   is_annealed=True,
+                   sim_num=2,
+                   q=3,
+                   x0=0.5,
+                   k=-10,
+                   m=(0, 1, 25),
+                   system_size=10000,
+                   time_horizon=1000,
+                   seed=1)
